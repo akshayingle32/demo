03ef8ed230390065edb626c248eced1f14e37a84
akshayingle32 <43000018+akshayingle32@users.noreply.github.com>
2018-09-05 16:35:29 +0530

    from server

--- array.php
+++ array.php
@@ -4,7 +4,7 @@
       <?php
          /* First method to create array. */
          $numbers = array( 1, 2, 3, 4, 5);
-         
+         echo "akshay";
          foreach( $numbers as $value ) {
             echo "Value is $value <br />";
          }
